42828ff2d63ca68c47b6fb919803b7391c6ae32f
jero0 <jrodriguezoc@unal.edu.co>
2025-09-04 18:07:37 -0500

    c

---
+++ AQM/Gram_Schmidt.py
@@ -0,0 +1,4 @@
+# Aqui se encuentra todo el algoritmo de ortogonalizacion de Gram Schmidt en el cual
+# se usan funciones establecidas en functions.py para facilitar el volumen de codigo y solo
+# permitir en este archivo el calculo sencillo de ortogonalizacion de bases.
+

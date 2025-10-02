e32ae3e26f83c901fa154e08d037f6558bad6c20
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-10-01 22:03:29 -0500

    c

--- AQM/Tarea3/Punto1.py
+++ AQM/Tarea3/Punto1.py
@@ -11,36 +11,57 @@ from scipy import integrate
 
 # Definimos las condiciones iniciales de tiempo y espacio
 
-x = np.linspace(-2,10,100)
+x = np.linspace(-3,10,600)
 xp = np.zeros_like(x)
 tp = np.zeros_like(x)
 m = 2
-time_scale = 1e-16
+time_scale = 1e-3
 
 
 # definimos la funcion de propagacion temporal pra una particula libre.
 
 def U(x,t,xp,tp):
 
-     prop = math.sqrt( m/(2*math.pi*1j*(t-tp)))*np.exp(1j*m*(x-xp)**2/(2*(t-tp)))
+     prop = np.sqrt( m/(2*math.pi*1j*(t)))*np.exp(1j*m*(x-xp)**2/(2*(t-tp)))
 
      return prop
 
 # Definimos la envolvente de la funcion de onda initial en t= 0,
 
-def phi_0(x,xp):
-    return np.where((-0.5 <= (x - xp)) & ((x - xp) <= 0.5), 1, 0)
+def phi_0(x,xp,p):
+    # return np.where((-0.5 <=(x -xp)) & ((x-xp) <= 0.5), np.exp(1j*p*x), 0)
+    return np.exp(-(x-xp)**2+1j*p*x)
+
+def phit(x,xp,t,tp):
+    phi_t = []
+    for i in x:
+        phi_t.append(integrate.simpson(phi_0(xp,0, 100)*U(i, t, xp, tp),xp))
+    return np.array(phi_t)
 
 # Update function
 def update(frame):
-    t = frame * time_scale
-    phi_t = integrate.simpson(phi_0(x,0),U(x,t,0,0))
-    ev = ax.plot(x,phi_t)
-    return ev
+    t = 2+ (frame+1) * time_scale
+    if  int(t/time_scale) <= 1:
+        ax.plot(x,np.abs(phi_0(x,0,10)))
+    else:
+        phi_t = phit(x,x,t,2)
+        ax.cla()  # clear axes
+        ax.plot(x, np.abs(phi_t / np.max(np.abs(phi_t))))
+
+    # Re-apply limits and labels (since cla() resets them)
+    ax.set_xlim(-3, 10)
+    ax.set_ylim(-1, 3)
+    ax.set_xlabel("x")
+    ax.set_ylabel("|ψ(x,t)|")
+    return []
 
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot()
 
-ani = animation.FuncAnimation(fig, update, frames=200, interval=50, blit=False)
+
+# phi_t = phit(x,x,0.1,0)
+# ax.plot(x,np.abs(phi_t/np.max(np.abs(phi_t))))
+
+ani = animation.FuncAnimation(fig, update, frames=1000, interval=20, blit=False)
 
 plt.show()
--- ejem.py
+++ ejem.py
@@ -1,47 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
-wavelength = 633e-9          # 633 nm
-k = 2 * np.pi / wavelength   # wave number
-kdir = k*np.array([[1,0,0]]).T
-omega = 3e8 * k              # angular frequency
-E0 = 1.0                     # amplitude
-H0 = 0.3
-
-no = 1.6557
-ne = 1.4849
+from scipy import integrate
 
 
-# Parameters of ellipsoid
-a, b, c = ne, ne, no
-
-# Create meshgrid
-u = np.linspace(0, 2 * np.pi, 100)
-v = np.linspace(0, np.pi, 100)
-u, v = np.meshgrid(u, v)
-
-# Parametric equations
-xel = a * np.cos(u) * np.sin(v)
-yel = b * np.sin(u) * np.sin(v)
-zel = c * np.cos(v)
-
-xef = no * np.cos(u) * np.sin(v)
-yef = no * np.sin(u) * np.sin(v)
-zef = no * np.cos(v)
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(zel, xel, yel, color="red", alpha=0.6)
-ax.plot_surface(zef, xef, yef, color="blue", alpha=0.6)
-
-ax.set_xlabel("Z")
-ax.set_ylabel("X")
-ax.set_zlabel("Y")
-
-ax.set_xlim(-3, 3)
-ax.set_ylim(-3, 3)
-ax.set_zlim(-3, 3)
-
-plt.show()

f271a979d5db3f172e838aa4ec87fcc547ad782a
juanes <jrodriguezoc@unal.edu.co>
2025-11-26 21:52:26 -0500

    c

--- ejem.py
+++ ejem.py
@@ -7,27 +7,36 @@ from matplotlib import colors, colors
 from matplotlib.pyplot import colormaps
 from sympy.codegen.ast import integer
 from sympy.vector import CoordSys3D
+import math
 
-r = CoordSys3D('r')
+R = CoordSys3D('r')
 
-nx , ny , alfa = sp.symbols('nx ny alfa', real=True)
+nx , ny = sp.symbols('nx ny', real=True)
 
-eav = (nx**2 + ny**2)/2
-del_av = (nx**2 - ny**2)/2
+lam = np.linspace(200e-9,700e-9,1000)
 
-n1_plus = sp.sqrt(alfa**2  + eav + sp.sqrt(del_av**2 + 4*alfa**2*eav))
-n1_minus = -sp.sqrt(alfa**2  + eav + sp.sqrt(del_av**2 + 4*alfa**2*eav))
-n2_plus = sp.sqrt(alfa**2  + eav - sp.sqrt(del_av**2 + 4*alfa**2*eav))
-n2_minus = -sp.sqrt(alfa**2  + eav - sp.sqrt(del_av**2 + 4*alfa**2*eav))
+k0 = 2*math.pi/lam
+nh = 1
+eav = 1.462**2
+del_av = 0.06*eav/2
+p = 500e-9
+alfa = lam/p
+n2 = np.sqrt(alfa**2 + eav - np.sqrt(del_av**2 + 4*eav*alfa**2+0j)+0j)
+h = 18e-6
+w = 1j*((eav+del_av)-n2**2-alfa**2)/(2*alfa*n2)
 
+t=  1/ (1j/2  * np.exp(-1j*k0*nh*h) *( (w-1j)**2*np.exp(-1j*k0*n2*h)- (w+1j)**2*np.exp(1j*k0*n2*h)))
+r= (w**2+1)*(1-np.exp(-1j*2*k0*n2*h))/(2*w*(1+np.exp(-1j*2*k0*n2*h)) -1j*(w**2-1)*(1-np.exp(-1j*2*k0*n2*h)) )
 
-A1 =  1*r.i -sp.I * (nx**2 - n1_plus**2 - alfa**2)/(2*alfa*n1_plus) *r.j
-A2 = 1*r.i -sp.I * (nx**2 - n1_minus**2 - alfa**2)/(2*alfa*n1_minus)*r.j
-A3 =1*r.i -sp.I * (nx**2 - n2_plus**2 - alfa**2)/(2*alfa*n2_plus)*r.j
-A4 = 1*r.i -sp.I * (nx**2 - n2_minus**2 - alfa**2)/(2*alfa*n2_minus)*r.j
+print(np.sqrt((700e-9/p)**2 + eav - np.sqrt(del_av**2 + 4*eav*(700e-9/p)**2+0j)+0j))
+fig1, ax1 = plt.subplots()
 
+ax1.plot(lam,np.abs(t)**2)
+# ax1.plot(lam,np.abs(r)**2)
 
-print(sp.simplify(sp.conjugate(A1).dot(A3)))
+plt.grid()
+plt.show()
+
 
 
 
